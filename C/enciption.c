25884a1aa3b7a982ce012f945ef954e8887e763b
SPQR14 <albertopicolara@gmail.com>
2020-12-30 09:59:40 -0600

    prueba configuración Clion

---
+++ C/enciption.c
@@ -0,0 +1,11 @@
+//
+// Created by Alberto on 30/12/2020.
+//
+
+#include "enciption.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("hola mundo");
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/keep_me_safe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/keep_me_safe.dir/C/enciption.c.obj"
+  "keep_me_safe.exe"
+  "keep_me_safe.exe.manifest"
+  "keep_me_safe.pdb"
+  "libkeep_me_safe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/keep_me_safe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
